85274339ab4959b0c1b6b5a71851f34c21e74ced
unknown <borlarhjhi@gmail.com>
2024-06-05 15:56:36 +0100

    Finished the Foundation3 and Foundation4 project

---
+++ final/Foundation4/Running.cs
@@ -0,0 +1,11 @@
+public class Running : Activity {
+    private double _distance;
+
+    public Running(string date, int length, double distance): base(date, length){
+        this._distance = distance;
+
+    }
+    override public double GetDistance(){
+        return this._distance;
+    }
+}
